3dbffc3f66cba6ed1c6a55c4f4fa4102c6454a9a
sagthorat <49110957+sagthorat@users.noreply.github.com>
2025-02-21 11:14:01 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,14 @@ variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
 }
+variable "tfc_aws_dynamic_credentials" {
+  description = "Object containing AWS dynamic credentials configuration"
+  type = object({
+    default = object({
+      shared_config_file = string
+    })
+    aliases = map(object({
+      shared_config_file = string
+    }))
+  })
+}
