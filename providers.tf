e42d49c58a28f965e8e5660234b42d8433ab6a09
Sagar Thorat <sagar.thorat@cenitex.vic.gov.au>
2025-02-22 12:31:21 +1100

    req prov

--- providers.tf
+++ providers.tf
@@ -1,6 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 provider "aws" {
   region              = "ap-southeast-4"
-  
+  shared_config_files = [var.tfc_aws_dynamic_credentials.default.shared_config_file]
 }
 
 provider "random" {}
