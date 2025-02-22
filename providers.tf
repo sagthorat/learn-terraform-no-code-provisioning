ccc786ea633826e7aae8edaf8afa470ccad28db9
Sagar Thorat <sagar.thorat@cenitex.vic.gov.au>
2025-02-22 12:05:04 +1100

    creds

--- providers.tf
+++ providers.tf
@@ -1,5 +1,6 @@
 provider "random" {}
+
 provider "aws" {
-  region              = "ap-southeast-4"
-  shared_config_files = [var.tfc_aws_dynamic_credentials.shared_config_file]
+  region                = "ap-southeast-4"
+  shared_credentials_file = var.tfc_aws_dynamic_credentials.shared_credentials_file
 }
