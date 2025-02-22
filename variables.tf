81dbd5b8ea180cf6c812f6d011d8bc0d072b0347
Sagar Thorat <sagar.thorat@cenitex.vic.gov.au>
2025-02-22 11:59:12 +1100

    0011

--- variables.tf
+++ variables.tf
@@ -1,5 +1,16 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
+variable "tfc_aws_dynamic_credentials" {
+  description = "Object containing AWS dynamic credentials configuration"
+  type = object({
+    default = object({
+      shared_config_file = string
+    })
+    aliases = map(object({
+      shared_config_file = string
+    }))
+  })
+}
 
 variable "db_name" {
   description = "Unique name to assign to RDS instance"
@@ -13,14 +24,4 @@ variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
 }
-variable "tfc_aws_dynamic_credentials" {
-  description = "Object containing AWS dynamic credentials configuration"
-  type = object({
-    default = object({
-      shared_config_file = string
-    })
-    aliases = map(object({
-      shared_config_file = string
-    }))
-  })
-}
+
